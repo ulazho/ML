4e1c9c0bd021961c77f37637bd5a466d49ae8c5f
Ulad <vlad.zholydev@gmail.com>
2022-12-01 22:31:38 +0100

    Home Task 6

---
+++ ML_HT_6/main.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures
+
+data = pd.read_csv("data6.tsv", sep='\t')
+data_y = pd.DataFrame(data['y'])
+
+
+data_x = pd.DataFrame(data['x'])
+model_x_1 = LinearRegression()
+model_x_1.fit(data_x, data_y.values.ravel())
+
+plt.plot(data_x, model_x_1.predict(data_x), "ro")
+plt.show()
+
+
+plt.plot(data_x, data_y, "bo")
+plt.show()
+
+poly_2 = PolynomialFeatures(2)
+data_x_2 = poly_2.fit_transform(data_x)
+
+
+model_x_2 = LinearRegression()
+
+
+model_x_2.fit(data_x_2, data_y.values.ravel())
+
+
+plt.plot(data_x, model_x_2.predict(data_x_2), "ro")
+plt.show()
+
+poly_5 = PolynomialFeatures(5)
+data_x_5 = poly_5.fit_transform(data_x)
+model_x_5 = LinearRegression()
+model_x_5.fit(data_x_5, data_y.values.ravel())
+plt.plot(data_x, model_x_5.predict(data_x_5), "ro")
+plt.show()
